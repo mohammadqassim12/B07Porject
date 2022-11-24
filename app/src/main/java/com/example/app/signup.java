ea3066ceab6e2af611ab218efa335ae2daeb6519
mohammadqassim12 <mohammadqassim000@gmail.com>
2022-11-24 02:06:32 -0500

    Activity for sign-in/up

--- app/src/main/java/com/example/app/signup.java
+++ app/src/main/java/com/example/app/signup.java
@@ -1,4 +1,15 @@
 package com.example.app;
 
-public class signup {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_screen);
+    }
 }
+
