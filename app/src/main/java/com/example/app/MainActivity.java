878931b51dc132cbaf04eba6a104e62498d5f2e8
rkwan05 <rkwan.hy@gmail.com>
2022-11-24 22:49:13 -0500

    intial admin setup, layouts, navigation, user input to database

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World from main branch!");
+        myRef.setValue("Hello, World from my branch!");
     }
 
     @Override
